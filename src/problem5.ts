41b5a4ae72949b788409442fb54ba94c16c33f35
junaeid <junaeidahmed979@gmail.com>
2024-11-06 22:05:05 +0600

    problem5 again

--- src/problem5.ts
+++ src/problem5.ts
@@ -1,18 +1,17 @@
 {
-// i used here keyOf property,also used generic form
+    // i used here keyOf property,also used generic form...basically mentors show me T & K but i used A & B
 
-
-interface Person{
-    name: string;
-    age: number;
-}
-function getProperty<T, K extends keyof T>(person:T, key:K): T[K]{
-    return person[key];
-} 
-const person: Person ={
-    name: 'Alice',
-    age:20,
-}
-// console.log(getProperty(person, "name"));
-// console.log(getProperty(person, 'age'))
+    interface Person {
+        name: string;
+        age: number;
+    }
+    function getProperty<A, B extends keyof A>(person: A, key: B): A[B] {
+        return person[key];
+    }
+    const person: Person = {
+        name: 'Alice',
+        age: 20,
+    }
+    console.log(getProperty(person, "name"));
+    // console.log(getProperty(person, 'age'))
 }
