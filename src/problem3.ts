0b4628c2aae60e635b6647261bdb85493e560c2f
junaeid <junaeidahmed979@gmail.com>
2024-11-07 02:05:42 +0600

    problem4

---
+++ src/problem3.ts
@@ -0,0 +1,6 @@
+{
+    function countWordOccurrences(str:string , val: string): number{
+            return str.split('').length
+    }
+    console.log(countWordOccurrences("TypeScript is great. I love TypeScript!", "typescript"));
+}   
--- src/problem4.ts
+++ src/problem4.ts
@@ -1,14 +1,41 @@
 {
 
+// first of all the question looks little bit tricky but i handle it by using type-guards
+interface Circle{
+    shape: "circle",
+    radius:number
+}
+interface Rectangle{
+    shape: "rectangle",
+    width: number,
+    height: number
+}
+type size =  Circle | Rectangle; 
+function calculateShapeArea(Shape: size){
+    if(Shape.shape === "circle"){
+        const value = Math.PI * (Shape.radius * Shape.radius);
+        return  value.toFixed(2)
+    }
+    else if(Shape.shape ==='rectangle'){
+        const value = (Shape.width * Shape.height);
+        return value
+    }
+    else{
+        return console.error(`Please use number`);
+        
+        
+    }
+
+}
 
 
-
-
-
-
-
-
-
-
+const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 4,
+    height: 6,
+  });
+console.log(circleArea);
+console.log(rectangleArea)
     
 }
