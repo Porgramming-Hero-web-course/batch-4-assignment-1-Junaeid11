05f685128ca0bde15e521cf1c048278e10786e65
junaeid <junaeidahmed979@gmail.com>
2024-11-06 20:51:11 +0600

    problem1

---
+++ src/problem1.ts
@@ -0,0 +1,14 @@
+{
+// i using here forOf loop to sum,when i used total =0 ,in function 0+1[0 index]=1,then 1+2[1 index]=3..continuously looping and final output 15
+
+function sumArray(number: number[]){
+    let total = 0;
+    number.forEach(num =>{
+        total += num
+    });
+    return total
+
+}
+// console.log(sumArray([1, 2, 3, 4, 5]));
+    
+}
