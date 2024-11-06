08b756ef5a465b80e95429e57dc813e4071f5ab7
junaeid <junaeidahmed979@gmail.com>
2024-11-07 04:16:41 +0600

    problem -03 solve by regEx

--- src/problem-3.ts
+++ src/problem-3.ts
@@ -1,7 +1,11 @@
 {
+
+// I used in filter or split and regular Regular expressions (/\W+/) which is matches non-word characters, i search it from google and i read all documentation about regEx how its work.then i appling op it
+
     function countWordOccurrences(sentence:string , word: string): number{
-           const words =sentence.toLowerCase().split(/\W/);
-           return words.filter(w => w === word.toLowerCase()).length;
+        const matchWord = sentence.toLowerCase().split(/\W+/);
+        const targetWord = word.toLowerCase();
+    return matchWord.filter(w => w === targetWord).length;
     }
     console.log(countWordOccurrences("TypeScript is great. I love TypeScript!", "typescript"));
 }   
