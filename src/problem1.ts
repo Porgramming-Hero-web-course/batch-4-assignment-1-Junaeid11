f9cf56c2c92025b7cbbb8e0860d3b209954be5a2
junaeid <junaeidahmed979@gmail.com>
2024-11-06 21:27:43 +0600

    problem2

--- src/problem1.ts
+++ src/problem1.ts
@@ -1,7 +1,7 @@
 {
-// i using here forOf loop to sum,when i used total =0 ,in function 0+1[0 index]=1,then 1+2[1 index]=3..continuously looping and final output 15
+// i using here forOf loop to sum,when i used total =0 ,in function 0+1[0 index]=1,then 1+2[1 index]=3..continuously looping and final output 
 
-function sumArray(number: number[]){
+function sumArray(number: number[]): number{
     let total = 0;
     number.forEach(num =>{
         total += num
--- src/problem2.ts
+++ src/problem2.ts
@@ -1,5 +1,12 @@
 {
+    // remove duplicate elements
+    //This code i do with using filter & indexOf Method...val=value,when filter method debug first property its also check index === value then its returns output 
 
 
-    
+
+    function removeDuplicates(num: number[]): number[] {
+        return num.filter((val, index) => num.indexOf(val) === index)
+    }
+    console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]))
+
 }
