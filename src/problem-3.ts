c8aa2c91b0ca898c6c4c7e7c74c682679ddb9dde
junaeid <junaeidahmed979@gmail.com>
2024-11-07 13:45:36 +0600

    assignment recheck

--- src/problem-3.ts
+++ src/problem-3.ts
@@ -1,11 +1,11 @@
 {
 
-// I used in filter or split and regular Regular expressions (/\W+/) which is matches non-word characters, i search it from google and i read all documentation about regEx how its work.then i appling op it
+    // I used in filter or split to doing this first i complete it toLowerCase() then sentence split then word tolowercase() the i filter it..thats it
 
-    function countWordOccurrences(sentence:string , word: string): number{
-        const matchWord = sentence.toLowerCase().split(/\W+/);
-        const targetWord = word.toLowerCase();
-    return matchWord.filter(w => w === targetWord).length;
+    function countWordOccurrences(sentence: string, word: string): number {
+        const firstSentence = sentence.toLowerCase().split(" ");
+        const lastWord = word.toLowerCase();
+        return firstSentence.filter(w => w === lastWord).length;
     }
     // console.log(countWordOccurrences("TypeScript is great. I love TypeScript!", "typescript"));
 }   
