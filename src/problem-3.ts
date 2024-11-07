b5c28e024bd3bafa609c3e24b369251ccae8c4f4
junaeid <junaeidahmed979@gmail.com>
2024-11-07 13:53:35 +0600

    complate

--- src/problem-3.ts
+++ src/problem-3.ts
@@ -1,11 +1,11 @@
 {
 
-    // I used in filter or split to doing this first i complete it toLowerCase() then sentence split then word tolowercase() the i filter it..thats it
+    // I used in filter or split to doing this first i complete it toLowerCase() then sentence split then i filter it and param length ..thats it
 
     function countWordOccurrences(sentence: string, word: string): number {
-        const firstSentence = sentence.toLowerCase().split(" ");
+        const firstSentence = sentence.toLowerCase();
         const lastWord = word.toLowerCase();
-        return firstSentence.filter(w => w === lastWord).length;
+        return firstSentence.split(" ").filter(param => param === lastWord).length;
     }
-    // console.log(countWordOccurrences("TypeScript is great. I love TypeScript!", "typescript"));
+    console.log(countWordOccurrences("TypeScript is great. I love TypeScript!", "typescript"));
 }   
